2f1023f740a28540e7d7518230d8cec8292ca159
walobwa <abelombonyo@gmail.com>
2023-01-06 15:08:26 +0300

    Added index and abot page

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const path = require('path');
 
 const PORT = process.env.PORT || 5000;
 
 
-
+//Create a static folder and point app to it
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => console.log('Server listening on port ' + PORT))
