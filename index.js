df043b04199b115987a08b313f54731d701c1846
walobwa <abelombonyo@gmail.com>
2023-01-06 15:48:58 +0300

    express handlebars

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 const express = require('express');
 const app = express();
+const exphbs = require('express-handlebars');
 const path = require('path');
 
 const PORT = process.env.PORT || 5000;
+
+//Set Handlebars Middleware
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+
+
+//Set handlerbar routes
+app.get('/', function (req,res) {
+    res.render('home');
+});
 
 
 //Create a static folder and point app to it
